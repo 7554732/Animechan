cf4f9db0cdaf02b2aaea3defc7b15b508951a5f7
Sergey Fomichev <7554732@gmail.com>
2023-03-18 17:47:44 +0300

    fix repository

--- app/src/main/java/com/fomichev/animechan/repository/AnimechanRepository.kt
+++ app/src/main/java/com/fomichev/animechan/repository/AnimechanRepository.kt
@@ -1,19 +1,11 @@
 package com.fomichev.animechan.repository
 
 import com.fomichev.animechan.network.AnimeApiService
-import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 class AnimechanRepository @Inject constructor(private val retrofitService: AnimeApiService){
     suspend fun getAnime(): List<Anime>{
         val result = retrofitService.getAnime10()
-//        val result = listOf(
-//            Anime("1"),
-//            Anime("2"),
-//            Anime("3"),
-//        )
-
-
         return result
     }
 }
